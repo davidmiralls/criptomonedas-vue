15c3cb3a52c71fd35dd14eebcc9dc66238bb1a74
David Miralles <davidmiralls@gmail.com>
2025-02-10 22:26:26 +0100

    commit final

---
+++ src/components/Cotizacion.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+defineProps({
+    cotizacion:{
+        type: Object,
+        required: true
+    }
+})
+
+
+
+</script>
+
+<template>
+
+<div class="contenedor-resultado">
+        <h2>Cotización</h2>
+        <div class="resultado">
+          <img 
+          :src="'https://cryptocompare.com/' + cotizacion.IMAGEURL" 
+          alt="imagen cripto">
+          <div>
+            <p>El precio es de: <span>{{  cotizacion.PRICE }}</span></p>
+            <p>Precio más alto del dia es de: <span>{{  cotizacion.HIGHDAY }}</span></p>
+            <p>Precio es de: <span>{{  cotizacion.LOWDAY }}</span></p>
+            <p>Variación últimas 24h: <span>{{  cotizacion.CHANGEPCT24HOUR }}%</span></p>
+            <p>ültima actualización: <span>{{  cotizacion.LASTUPDATE }}</span></p>
+          </div>
+        </div>
+      </div>
+
+
+</template>
